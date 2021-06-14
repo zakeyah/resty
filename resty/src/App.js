2c48cd2fb1cae116aba6b23404ed067c00b99bf3
zakeyah <mom11omar@gmail.com>
2021-06-14 18:58:36 +0300

    lab27

--- resty/src/App.js
+++ resty/src/App.js
@@ -4,16 +4,52 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Form from './Form';
+import Results from './Results'
 // import './App.css';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header/>
-      <Form data="ffffff"/>
-      <Footer/>
-    </React.Fragment>
-  );
+
+
+
+
+
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <Header/>
+//       <Form data="ffffff"/>
+//       <Footer/>
+//     </React.Fragment>
+//   );
+// }
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      headers:{},
+      // count:0,
+      results:{},
+      
+    }
+  }
+
+  handleForm=(data, headers) => {
+    // console.log('results...........',headers,'data......',data)
+    this.setState({results:data.body, headers:headers});
+    // console.log(this.state)
+}
+
+  render(){
+      return (
+        <>
+      
+          <Header/>
+          <Form  handler={this.handleForm}/>
+          <Results data={this.state}  />
+          <Footer/>
+        </>
+      );
+ }
+
 }
 
 export default App;
